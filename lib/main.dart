96af63f2f470770b1105dfe7638e2b83f12e07b8
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2021-03-07 02:29:00 -0300

    Changes on API and plugin
    downgrade in the http plugin because it was generating an error and incompatibility in the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_blocpattern/screens/home.dart';
 
+import 'api.dart';
+
 void main() {
+  Api api = Api();
+  api.search("eletro");
   runApp(MyApp());
 }
 
