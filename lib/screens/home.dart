c02025bdbc0da94b1a049beb3fbdf328a961d56e
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2021-03-07 03:05:42 -0300

    main screen layout
    - added main screen layout
    - changed AppBar image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,35 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/youtubelogo.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget> [
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            color: Colors.red,
+            onPressed: (){
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
